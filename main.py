5385c4f99dd3b850623ff754370243565a8a3fef
YoonJinJung <yj2135589@g.skku.edu>
2023-03-09 10:47:42 +0000

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+    for i in range(len(nums)-1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                res = [i, j]
+    
+    return res
